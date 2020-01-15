d17c65ed3b22553ec45fe485449010325588dc8c
alexbetricks <alex@betricks.com.br>
2020-01-15 11:21:40 -0300

    backend aula 02

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-amu8e.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json());
+
+app.use(routes);
+
+// Métodos GET, POST, PUT, DELETE
+
+// Tipos de parametros
+
+// Query Params: request.query(Filtros, Ordenação, paginação, ...)
+// Routs Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro) 
+
+// MongoDB (Não-relacional)
+
+
+app.listen(3333);
